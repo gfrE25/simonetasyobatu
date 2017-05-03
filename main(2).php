6e1689d174d083e6e30bd2d82a76456d004ce28a
gamesukimanIRS <gamesukiman@yahoo.co.jp>
2017-05-03 17:43:17 +0900

    Update main(2).php

--- main(2).php
+++ main(2).php
@@ -17,15 +17,13 @@ class main extends PluginBase implements Listener{
 	}
 	public function onSignChange(SignChangeEvent $event){
 		$text = $event->getLines();
+		$name = $event->getPlayer()->getName();
+		$today = date("m/d");
 		if(不適切な内容のため消させていただきます){
 			$player = $event->getPlayer()->getName();
 			$event->setLine(0,"§6下ネタはやめて下さい");
 			$player->kick("§cあなたの書いた看板を下ネタと判断しました", false);
 		}
-	function onSignChange(SignChangeEvent $event){
-		$text = $event->getLines();
-		$name = $event->getPlayer()->getName();
-		$today = date("m/d");
 		if($text[0] == "bukken-koudou"){
 			$event->setLine(0,"§l§6⚠§r§c警告:違法物件§l§6⚠");
 			$event->setLine(1,"§a公道から最低でも1マス離して下さい");
