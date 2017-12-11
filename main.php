f375157fc8268373bcfa7ce3a02011966888f5d4
gfrE25 <34418779+gfrE25@users.noreply.github.com>
2017-12-11 18:40:08 +0900

    コード整

--- main.php
+++ main.php
@@ -1,38 +1,31 @@
 <?php
 
-namespace simonetasyobatu;
+//皆さんでお花を摘みに行きましょう
 
-use pocketmine\event\block\SignChangeEvent;
+
+namespace simonetayobou;
+
+use pocketmine\Player;
 use pocketmine\event\Listener;
+use pocketmine\event\block\SignChangeEvent;
 use pocketmine\plugin\PluginBase;
 
-class main extends PluginBase implements Listener{
-
-	public function onEnable(){
+class main extends PluginBase implements Listener
+{
+  
+	public function onEnable()
+        {
     	$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
-
-	public function onSignChange(SignChangeEvent $event){
-		$lines = $event->getLines();
-		switch($lines[0]){
-			//下のcaseは気分が悪くなる恐れがありますのでご注意ください
-			//以下の単語はgamesukimanIRSさんによって書かれました
-			case 'oppai':
-			case 'おっぱい':
-			case 'sex':
-			case 'セックス':
-			case 'tinko':
-			case 'ちんこ':
-			case 'manko':
-			case 'まんこ':
-			case 'sikosiko':
-			case 'しこしこ':
-			case 'シコシコ':
-			case '4545':
-				$event->setLine(0,"§6下ネタはやめて下さい");
-				$event->getPlayer()->kick("§cあなたの書いた看板を下ネタと判断しました", false);
-				break;
-		}
-	}
-
+	
+	public function onSignChange(SignChangeEvent $event)
+	{
+           $lines = $event->getLines();
+	   $words = array("おsex", "おtinko","おmanko","おsikosiko","Oh~sikosiko");
+           foreach($words as $search){
+              if($lines[0] === $search){
+	         $event->getPlayer()->kick("§cあなたの書いた看板を下ネタと判断しました", false);
+	      }
+	   }
+        }
 }
